53ae75733013b6d5ede914ed76b7e1713d02598d
Slasher <Slasher@SlasherMachine.home>
2015-12-16 20:44:37 +0100

    zz

--- Aukcje/src/controllers/aukcje/ListaAukcji.java
+++ Aukcje/src/controllers/aukcje/ListaAukcji.java
@@ -1,8 +1,15 @@
 package controllers.aukcje;
 
 import main.ServletMain;
+import modules.aukcje.Aukcja;
 
 public class ListaAukcji extends ServletMain 
 {
+	protected Aukcja aukcje[];
 	
+	public ListaAukcji() {
+		super();
+		
+		page_url = page_url = "views/ListaAukcji.html";
+	}
 }

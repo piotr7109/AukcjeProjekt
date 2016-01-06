64e55d2c79092113007a72ae0906e2a1af426388
Piotr <piotr1_krk@interia.pl>
2016-01-06 17:38:18 +0100

    dodany podgląd

--- Aukcje/src/controllers/aukcje/PodgladAukcji.java
+++ Aukcje/src/controllers/aukcje/PodgladAukcji.java
@@ -11,6 +11,7 @@ import main.ServletMain;
 import modules.aukcje.Aukcja;
 import modules.aukcje.AukcjaFactory;
 import modules.przedmioty.Przedmiot;
+import modules.przedmioty.PrzedmiotFactory;
 
 public class PodgladAukcji extends ServletMain 
 {
@@ -33,6 +34,11 @@ public class PodgladAukcji extends ServletMain
 		AukcjaFactory a_factory = new AukcjaFactory();
 		a_factory.setId(id_aukcji);
 		aukcja = (Aukcja)a_factory.getObject();
+		
+		PrzedmiotFactory p_factory = new PrzedmiotFactory();
+		p_factory.setId(aukcja.getIdPrzedmiotu());
+		aukcja.setPrzedmiot((Przedmiot)p_factory.getObject());
+		
 		if(testy() == "AUKCJA_NIE_ISTNIEJE")
 		{
 			html = Komunikaty.getError("Aukcja o podanym identyfikatorze nie istnieje!");
@@ -46,10 +52,12 @@ public class PodgladAukcji extends ServletMain
     }
 	private void getRightHtml()
 	{
+		
 		Przedmiot przedmiot = aukcja.getPrzedmiot();
-		html = String.format(this.getHtml(page_url), this.aukcja.getNazwa(),
+		html = String.format(this.getHtml(page_url), this.aukcja.getNazwa(), przedmiot.getNazwa(),
 				"http://www.dfv.pl/tl_files/dfv/Poradniki/Szkola_dfv/Jak_zrobi%C5%82em_to_zdj%C4%99cie/Zabaewa/P1352dfv.jpg", 
-				"aktualna_cena","ostatnie_przebicie",aukcja.getDataZakonczenia(), aukcja.getId(), przedmiot.getOpis());
+				"aktualna_cena","ostatnie_przebicie",aukcja.getDataZakonczenia(), aukcja.getId(), 
+				 przedmiot.getOpis());
 	}
 	private String testy()
 	{

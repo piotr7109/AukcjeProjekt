98fc63f370342db4d52ef4c68398368241ddb1ab
BardzkiM <michalbardzki@gmail.com>
2016-01-06 14:16:07 +0100

    commity

--- Aukcje/src/modules/uzytkownicy/UzytkownicyLista.java
+++ Aukcje/src/modules/uzytkownicy/UzytkownicyLista.java
@@ -14,8 +14,19 @@ public class UzytkownicyLista extends AbstractLista
 	
 	protected Object fetchObject(ResultSet rs) throws SQLException
 	{
+		//id login haslo imie nazwisko email adres stan_konta status bledne_logowanie
 		Uzytkownik u = new Uzytkownik();
 		u.setId(rs.getInt("id"));
+		u.setLogin(rs.getString("login"));
+		u.setHaslo(rs.getString("haslo"));
+		u.setImie(rs.getString("imie"));
+		u.setNazwisko(rs.getString("nazwisko"));
+		u.setEmail(rs.getString("email"));
+		u.setStanKonta(rs.getInt("stan_konta"));
+		u.setStatus(rs.getString("status").charAt(0));
+		u.setBledneLogowania(rs.getInt("bledne_logowania"));
+		
+		
 		
 		return u;
 	}

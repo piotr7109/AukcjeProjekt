0f368ddf72ab6d8d071db5707e492ad4a500fa7a
BardzkiM <michalbardzki@gmail.com>
2016-01-06 14:49:32 +0100

    comcio

--- Aukcje/src/controllers/aukcje/PodgladAukcji.java
+++ Aukcje/src/controllers/aukcje/PodgladAukcji.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import main.ServletMain;
+import modules.aukcje.Aukcja;
+import modules.aukcje.AukcjaFactory;
 
 public class PodgladAukcji extends ServletMain 
 {
@@ -14,6 +16,7 @@ public class PodgladAukcji extends ServletMain
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private int id_aukcji;
 	
 	public PodgladAukcji() 
 	{
@@ -22,6 +25,16 @@ public class PodgladAukcji extends ServletMain
 	}
 	public void doRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
+		id_aukcji = Integer.getInteger(request.getParameter("id_aukcji"));
+		// pobraæ obiek z bazy
+		AukcjaFactory a_factory = new AukcjaFactory();
+		a_factory.setId(id_aukcji);
+		Aukcja aukcja = (Aukcja)a_factory.getObject();
+		
+		
+		
+		
+		
 		html = this.getHtml(page_url);
     	initServlet();
     }

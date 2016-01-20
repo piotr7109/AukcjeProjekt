a2949202f81a01a64ab702a2b54cb1b0928fc3bd
BardzkiM <michalbardzki@gmail.com>
2016-01-20 21:43:46 +0100

    zdjęcia

--- Aukcje/src/controllers/aukcje/ListaAukcji.java
+++ Aukcje/src/controllers/aukcje/ListaAukcji.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import main.ServletMain;
 import modules.aukcje.Aukcja;
 import modules.aukcje.AukcjaLista;
+import modules.przedmioty.Przedmiot;
+import modules.przedmioty.PrzedmiotFactory;
 
 public class ListaAukcji extends ServletMain 
 {
@@ -38,12 +40,21 @@ public class ListaAukcji extends ServletMain
     {
     	String html ="";
     	Aukcja au;
+    	
     	for(int i=0; i< this.ile_aukcji; i++)
     	{
     		au = (Aukcja)aukcje.get(i);
+    		PrzedmiotFactory p_factory = new PrzedmiotFactory();
+    		p_factory.setId(au.getIdPrzedmiotu());
+    		System.out.println(au.getIdPrzedmiotu());
+    		au.setPrzedmiot((Przedmiot)p_factory.getObject());
+    		System.out.println(au.getPrzedmiot().getZdjecieSrc());
+    		
+    		
     		html +="<tr>";
     		
     		html +=String.format("<td>%d</td>", (i+1));
+    		html +=String.format("<td>%s</td>", "<img src = " + au.getPrzedmiot().getZdjecieSrc() + " width=\"300\" height=\"150\" border=\"3\" </img>");
     		html +=String.format("<td>%s</td>", au.getNazwa());
     		html +=String.format("<td>%s</td>", au.getDataZakonczenia());
     		html +=String.format("<td><a href=podglad_aukcji?id_aukcji=%d><span class='glyphicon glyphicon-search'></span></a></td>", au.getId());
--- Aukcje/src/controllers/aukcje/PodgladAukcji.java
+++ Aukcje/src/controllers/aukcje/PodgladAukcji.java
@@ -174,7 +174,7 @@ public class PodgladAukcji extends ServletMain
 	{
 		String html = "";
 		Przedmiot przedmiot = aukcja.getPrzedmiot();
-		html = String.format(this.getHtml(page_url), this.aukcja.getNazwa(), przedmiot.getNazwa(), "/img/obr_1.jpg", aktualna_cena+" pkt BICK", data_ostatniego_przebicia, aukcja.getDataZakonczenia(), aukcja.getId(),
+		html = String.format(this.getHtml(page_url), this.aukcja.getNazwa(), przedmiot.getNazwa(), przedmiot.getZdjecieSrc(), aktualna_cena+" pkt BICK", data_ostatniego_przebicia, aukcja.getDataZakonczenia(), aukcja.getId(),
 				przedmiot.getOpis());
 		return html;
 	}

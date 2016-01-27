ec533c1b414f67a5fb0baa1610eddcade077e1fa
Piotr <piotr1_krk@interia.pl>
2016-01-27 18:04:12 +0100

    commit

--- Aukcje/src/controllers/aukcje/ListaAukcji.java
+++ Aukcje/src/controllers/aukcje/ListaAukcji.java
@@ -53,7 +53,7 @@ public class ListaAukcji extends ServletMain
 			html += "<tr>";
 
 			html += String.format("<td>%d</td>", (ile_aukcji-i));
-			html += String.format("<td>%s</td>", "<img src = " + au.getPrzedmiot().getZdjecieSrc() + " width=\"300\" height=\"150\" border=\"3\" </img>");
+			html += String.format("<td>%s</td>", String.format("<img src ='%s' class='lista_aukcji_img'  border='3' >", au.getPrzedmiot().getZdjecieSrc()));
 			html += String.format("<td>%s</td>", au.getNazwa());
 			html += String.format("<td>%s</td>", au.getDataZakonczenia());
 			html += String.format("<td><a href=podglad_aukcji?id_aukcji=%d><span class='glyphicon glyphicon-search'></span></a></td>", au.getId());

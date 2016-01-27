a3ac512c367711314bc6477e02e419e29553d0b3
Piotr <piotr1_krk@interia.pl>
2016-01-27 22:26:41 +0100

    TinyMCE

--- Aukcje/src/modules/przedmioty/Przedmiot.java
+++ Aukcje/src/modules/przedmioty/Przedmiot.java
@@ -1,5 +1,7 @@
 package modules.przedmioty;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,49 +14,93 @@ public class Przedmiot
 	private String nazwa;
 	private String opis;
 	private String zdjecie_src;
+
 	public int getId()
 	{
 		return id;
 	}
+
 	public void setId(int id)
 	{
 		this.id = id;
 	}
+
 	public String getNazwa()
 	{
 		return nazwa;
 	}
+
 	public void setNazwa(String nazwa)
 	{
 		this.nazwa = nazwa;
 	}
+
 	public String getOpis()
 	{
 		return opis;
 	}
+
 	public void setOpis(String opis)
 	{
 		this.opis = opis;
 	}
+
 	public String getZdjecieSrc()
 	{
 		return zdjecie_src;
 	}
+
 	public void setZdjecieSrc(String zdjecie_src)
 	{
 		this.zdjecie_src = zdjecie_src;
 	}
-	
+
 	public void insertPrzedmiot()
 	{
-		
+
 		PostgreSQLJDBC pgsq = new PostgreSQLJDBC();
-		String query = String.format("INSERT INTO t_przedmioty(nazwa, opis, zdjecie_src)"
-									+" VALUES ('%s','%s','%s')",
-										nazwa, opis, zdjecie_src);
-		pgsq.queryOpertaion(query);
+		PreparedStatement stmt = null;
+		Connection c = pgsq.getC();
+		try
+		{
+			
+			stmt = c.prepareStatement("INSERT INTO t_przedmioty(nazwa, opis, zdjecie_src) values (?, ?, ?)");
+			stmt.setString(1, nazwa);
+			stmt.setString(2, opis);
+			stmt.setString(3, zdjecie_src);
+			stmt.executeUpdate();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if (stmt != null)
+				{
+					stmt.close();
+				}
+			}
+			catch (Exception e)
+			{
+				// log this error
+			}
+			try
+			{
+				if (c != null)
+				{
+					c.close();
+				}
+			}
+			catch (Exception e)
+			{
+				// log this error
+			}
+		}
 	}
-	
+
 	public int getLastId()
 	{
 		int id_przedmiotu = 0;
@@ -63,36 +109,34 @@ public class Przedmiot
 		try
 		{
 			Statement stmt = pgsq.getC().createStatement();
-			
-			ResultSet rs = stmt.executeQuery( query );
-			while ( rs.next() ) 
+
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next())
 			{
 				id_przedmiotu = rs.getInt("id");
-	            
+
 			}
 			rs.close();
 		}
-		catch(SQLException e)
+		catch (SQLException e)
 		{
-			
+
 		}
 		return id_przedmiotu;
 	}
+
 	public void updatePrzedmiot()
 	{
 		PostgreSQLJDBC pgsq = new PostgreSQLJDBC();
-		String query = String.format("UPDATE t_przedmioty SET nazwa = '%s', opis = '%s', zdjecie_src = '%s' WHERE id=%d",
-									nazwa, opis, zdjecie_src, id);
+		String query = String.format("UPDATE t_przedmioty SET nazwa = '%s', zdjecie_src = '%s' WHERE id=%d", nazwa, zdjecie_src, id);
 		pgsq.queryOpertaion(query);
 	}
+
 	public void deletePrzedmiot()
 	{
 		PostgreSQLJDBC pgsq = new PostgreSQLJDBC();
 		String query = String.format("DELETE FROM t_przedmioty WHERE id=%d", id);
 		pgsq.queryOpertaion(query);
 	}
-	
-	
-	
-	
+
 }
